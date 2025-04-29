2df628bdfe19629fe0c7f4404f9d081923f05227
GabrielBuergo <gabriel.buergo@fsti360.com>
2025-04-29 17:28:37 -0400

    Subir

--- SFB.Vue/src/main.js
+++ SFB.Vue/src/main.js
@@ -1,33 +1,6 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
-import { router } from './router'
 
-// Plugins
-import { PerfectScrollbarPlugin } from 'vue3-perfect-scrollbar'
-import VueTablerIcons from 'vue-tabler-icons'
-import VueApexCharts from 'vue3-apexcharts'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css'
-
-// Estilos
-import '@/scss/style.scss'
-import '@fontsource/public-sans/400.css'
-import '@fontsource/public-sans/500.css'
-import '@fontsource/public-sans/600.css'
-import '@fontsource/public-sans/700.css'
-
-// Mock API
-import { fakeBackend } from '@/utils/helpers/fake-backend'
-
-const app = createApp(App)
-fakeBackend()
-
-app.use(router)
-app.use(PerfectScrollbarPlugin)
-app.use(createPinia())
-app.use(VueTablerIcons)
-app.use(Antd)
-app.use(VueApexCharts)
-
-app.mount('#app')
+createApp(App).mount('#app')
--- sfb.vue/eslint.config.js
+++ sfb.vue/eslint.config.js
@@ -1,18 +1,24 @@
-import globals from 'globals';
-import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginVue from 'eslint-plugin-vue';
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
 
-export default [
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
   {
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        parser: '@typescript-eslint/parser'
-      }
-    }
+      globals: {
+        ...globals.browser,
+      },
+    },
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential']
-];
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+])
