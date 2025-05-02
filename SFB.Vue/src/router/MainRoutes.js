2d9e9497696173801258347c4591911c2d7d0155
ING-BUERGO-GABRIEL <gabrielbuergo@gmail.com>
2025-05-01 23:18:44 -0400

    Subir Login

--- SFB.Vue/src/router/MainRoutes.js
+++ SFB.Vue/src/router/MainRoutes.js
@@ -1,45 +1,45 @@
 export default {
-    path: '/main',
-    meta: {
-        requiresAuth: true
+  path: '/main',
+  meta: {
+    requiresAuth: true
+  },
+  redirect: '/main',
+  component: () => import('@/layouts/dashboard/DashboardLayout.vue'),
+  children: [
+    //{
+    //  name: 'LandingPage',
+    //  path: '/',
+    //  component: () => import('@/views/dashboard/DefaultDashboard.vue')
+    //},
+    {
+      name: 'Dashboard',
+      path: '/dashboard',
+      component: () => import('@/views/dashboard/DefaultDashboard.vue')
     },
-    redirect: '/main',
-    component: () => import('@/layouts/dashboard/DashboardLayout.vue'),
-    children: [
-        {
-            name: 'LandingPage',
-            path: '/',
-            component: () => import('@/views/dashboard/DefaultDashboard.vue')
-        },
-        {
-            name: 'Dashboard',
-            path: '/dashboard',
-            component: () => import('@/views/dashboard/DefaultDashboard.vue')
-        },
-        {
-            name: 'Typography',
-            path: '/typography',
-            component: () => import('@/views/typography/TypographyPage.vue')
-        },
-        {
-            name: 'Colors',
-            path: '/colors',
-            component: () => import('@/views/colors/ColorPage.vue')
-        },
-        {
-            name: 'Shadow',
-            path: '/shadow',
-            component: () => import('@/views/shadows/ShadowPage.vue')
-        },
-        {
-            name: 'Color',
-            path: '/icon/ant',
-            component: () => import('@/views/icons/AntDesignIcons.vue')
-        },
-        {
-            name: 'other',
-            path: '/sample-page',
-            component: () => import('@/views/StarterPage.vue')
-        }
-    ]
+    {
+      name: 'Typography',
+      path: '/typography',
+      component: () => import('@/views/typography/TypographyPage.vue')
+    },
+    {
+      name: 'Colors',
+      path: '/colors',
+      component: () => import('@/views/colors/ColorPage.vue')
+    },
+    {
+      name: 'Shadow',
+      path: '/shadow',
+      component: () => import('@/views/shadows/ShadowPage.vue')
+    },
+    {
+      name: 'Color',
+      path: '/icon/ant',
+      component: () => import('@/views/icons/AntDesignIcons.vue')
+    },
+    {
+      name: 'other',
+      path: '/sample-page',
+      component: () => import('@/views/StarterPage.vue')
+    }
+  ]
 }
