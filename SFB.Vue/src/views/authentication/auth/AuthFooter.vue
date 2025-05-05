eca4fd418286bcebdd30cd6984fd9006b4f6ef77
GabrielBuergo <gabriel.buergo@fsti360.com>
2025-05-05 09:07:32 -0400

    Login

--- SFB.Vue/src/views/authentication/auth/AuthFooter.vue
+++ SFB.Vue/src/views/authentication/auth/AuthFooter.vue
@@ -1,15 +1,9 @@
-<script setup lang="ts">
+<script setup>
 import { shallowRef } from 'vue';
 
 const footerLink = shallowRef([
   {
-    title: 'Terms and Conditions'
-  },
-  {
-    title: 'Privacy Policy'
-  },
-  {
-    title: 'CA Privacy Notice'
+    title: 'Términos y condiciones'
   }
 ]);
 </script>
@@ -18,8 +12,8 @@ const footerLink = shallowRef([
     <v-row justify="center" no-gutters>
       <v-col cols="12" md="6" class="text-md-left text-center">
         <p class="text-subtitle-2 text-lightText mb-md-0 mb-4">
-          This site is protected by
-          <a href="/" class="text-primary">Privacy Policy</a>
+          Este sitio está protegido por
+          <a href="/" class="text-primary">Política de Privacidad</a>
         </p>
       </v-col>
       <v-col class="d-flex flex-md-row flex-column justify-md-end align-center" cols="12" md="6">
@@ -27,7 +21,7 @@ const footerLink = shallowRef([
           v-for="(item, i) in footerLink"
           :key="i"
           class="mx-md-3 mx-2 mb-md-0 mb-2 text-subtitle-2 text-lightText"
-          href="https://codedthemes.com"
+          href="/"
           target="_blank"
         >
           {{ item.title }}
