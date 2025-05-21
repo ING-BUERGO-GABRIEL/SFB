d5b7ec106b668c44a69f474673b55d1db183b3f2
GabrielBuergo <gabriel.buergo@fsti360.com>
2025-05-21 13:42:08 -0400

    Edicion de productos

--- SFB.Vue/src/views/modules/iaw/products/ProductsList.vue
+++ SFB.Vue/src/views/modules/iaw/products/ProductsList.vue
@@ -1,6 +1,9 @@
 <template>
-  <Header />
+  <HeaderBar />
   <v-row class="mb-0">
+     <v-col cols="12" md="8">
+      <ProductEdit/>
+    </v-col>
     <v-col cols="12" md="8">
       <DataTable />
     </v-col>
@@ -10,7 +13,8 @@
   </v-row>
 </template>
 <script setup>
-import Header from './components/HeaderBar.vue';
+import HeaderBar from './components/HeaderBar.vue';
 import DataTable from './components/DataTable.vue';
 import AnalyticsReport from './components/AnalyticsReport.vue';
+import ProductEdit from './components/ProductEdit.vue';
 </script>
