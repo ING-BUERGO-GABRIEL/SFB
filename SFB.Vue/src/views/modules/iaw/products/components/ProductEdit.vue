9760e6f189df08ac2e80c307cbb6dec2e015a685
GabrielBuergo <gabriel.buergo@fsti360.com>
2025-05-22 13:47:48 -0400

    Crear Modal

--- SFB.Vue/src/views/modules/iaw/products/components/ProductEdit.vue
+++ SFB.Vue/src/views/modules/iaw/products/components/ProductEdit.vue
@@ -1,100 +1,29 @@
 <template>
-  <div class="text-center pa-4">
-    <v-dialog v-model="dialog" transition="dialog-bottom-transition" width="100%" max-width="100%">
-      <template v-slot:activator="{ props: activatorProps }">
-        <v-btn
-          prepend-icon="mdi-cog"
-          size="small"
-          text="Settings"
-          v-bind="activatorProps"
-        ></v-btn>
-      </template>
-
-      <v-card>
-        <v-toolbar>
-          <v-btn icon="mdi-close" @click="dialog = false"></v-btn>
-
-          <v-toolbar-title>Settings</v-toolbar-title>
-
-          <v-toolbar-items>
-            <v-btn text="Save" variant="text" @click="dialog = false"></v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-
-        <v-list lines="two">
-          <v-list-subheader>User Controls</v-list-subheader>
-
-          <v-list-item
-            subtitle="Set the content filtering level to restrict apps that can be downloaded"
-            title="Content filtering"
-            link
-          ></v-list-item>
-
-          <v-list-item
-            subtitle="Require password for purchase or use password to restrict purchase"
-            title="Password"
-            link
-          ></v-list-item>
-
-          <v-divider></v-divider>
-
-          <v-list-subheader>General</v-list-subheader>
-
-          <v-list-item
-            subtitle="Notify me about updates to apps or games that I downloaded"
-            title="Notifications"
-            @click="notifications = !notifications"
-          >
-            <template v-slot:prepend>
-              <v-list-item-action start>
-                <v-checkbox-btn
-                  v-model="notifications"
-                  color="primary"
-                ></v-checkbox-btn>
-              </v-list-item-action>
-            </template>
-          </v-list-item>
-
-          <v-list-item
-            subtitle="Auto-update apps at any time. Data charges may apply"
-            title="Sound"
-            @click="sound = !sound"
-          >
-            <template v-slot:prepend>
-              <v-list-item-action start>
-                <v-checkbox-btn
-                  v-model="sound"
-                  color="primary"
-                ></v-checkbox-btn>
-              </v-list-item-action>
-            </template>
-          </v-list-item>
-
-          <v-list-item
-            subtitle="Automatically add home screen widgets"
-            title="Auto-add widgets"
-            @click="widgets = !widgets"
-          >
-            <template v-slot:prepend>
-              <v-list-item-action start>
-                <v-checkbox-btn
-                  v-model="widgets"
-                  color="primary"
-                ></v-checkbox-btn>
-              </v-list-item-action>
-            </template>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-dialog>
+  <div class="product-edit-wrapper">
+    <div class="full-cover">
+      <h1>Hola</h1>
+    </div>
   </div>
 </template>
 
 <script setup>
-  import { shallowRef } from 'vue'
-
-  const dialog = shallowRef(false)
-  const notifications = shallowRef(false)
-  const sound = shallowRef(true)
-  const widgets = shallowRef(false)
+// sin cambios aquí
 </script>
+
+<style scoped>
+.product-edit-wrapper {
+  position: relative;
+  width: 100%;
+  height: 100%; /* heredado por fill-height */
+}
+
+.full-cover {
+  position: absolute;
+  inset: 0;                /* equivale a top:0; right:0; bottom:0; left:0 */
+  background-color: blue;
+  display: flex;           /* opcional: centra el contenido */
+  align-items: center;
+  justify-content: center;
+  color: white;            /* para que se lea “Hola” */
+}
+</style>
