29e9754b3f7fa5d125b0cbc36f69239a86deafff
GabrielBuergo <gabriel.buergo@fsti360.com>
2025-09-12 13:48:09 -0400

    styock

---
+++ SFB.Infrastructure/Entities/IAW/EProductStock.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SFB.Infrastructure.Entities.IAW
+{
+    [Table("IAW_ProductStock")]
+    public class EProductStock
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int NroStock { get; set; }
+
+        [Required]
+        public int NroProduct { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Stock { get; set; }
+
+        [NotMapped]
+        public virtual EProduct? Product { get; set; }
+    }
+}
