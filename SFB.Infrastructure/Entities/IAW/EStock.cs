a475b14bbd80a42a9f1a866197e848de4a7b4291
ING-BUERGO-GABRIEL <gabrielbuergo@gmail.com>
2025-09-14 21:19:35 -0400

    stock

---
+++ SFB.Infrastructure/Entities/IAW/EStock.cs
@@ -0,0 +1,37 @@
+﻿using SFB.Infrastructure.Entities.CMS;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SFB.Infrastructure.Entities.IAW
+{
+    [Table("IAW_Stock")]
+    public class EStock : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int StockId { get; set; }
+
+        [Required, ForeignKey(nameof(Product))]
+        public int NroProduct { get; set; }
+        public EProduct Product { get; set; }
+
+        [Required, ForeignKey(nameof(Warehouse))]
+        public int WarehouseId { get; set; }
+        public EWarehouse Warehouse { get; set; }
+
+        // Cantidad disponible física
+        [Required]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal QtyOnHand { get; set; }
+
+        // Cantidad comprometida (reservas en ventas, etc.)
+        [Required]
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal QtyReserved { get; set; }
+
+        // Punto de reorden opcional
+        [Column(TypeName = "decimal(18,4)")]
+        public decimal? ReorderPoint { get; set; }
+
+    }
+}
---
+++ SFB.Infrastructure/Entities/IAW/EWarehouse.cs
@@ -0,0 +1,23 @@
+﻿using SFB.Infrastructure.Entities.CMS;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SFB.Infrastructure.Entities.IAW
+{
+    [Table("IAW_Warehouse")]
+    public class EWarehouse : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WarehouseId { get; set; }
+
+        [Required, MaxLength(120)]
+        public string Name { get; set; }
+
+        [MaxLength(250)]
+        public string? Location { get; set; }
+
+        [Required]
+        public bool Status { get; set; } = true;
+    }
+}
