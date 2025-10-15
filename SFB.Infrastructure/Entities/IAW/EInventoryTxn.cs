aace58f724baa1445f29575ecfc88f99de378f04
GabrielBuergo <gabriel.buergo@fsti360.com>
2025-10-15 13:45:37 -0400

    mod txn

--- SFB.Infrastructure/Entities/IAW/EInventoryTxn.cs
+++ SFB.Infrastructure/Entities/IAW/EInventoryTxn.cs
@@ -11,9 +11,14 @@ namespace SFB.Infrastructure.Entities.IAW
     [Table("IAW_InventoryTxn")]
     public class EInventoryTxn
     {
+        [Key]
         [Required]
         public int TxnId { get; set; }
 
         public int? TxnOrigin { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string ModOrigin { get; set; }
     }
 }
