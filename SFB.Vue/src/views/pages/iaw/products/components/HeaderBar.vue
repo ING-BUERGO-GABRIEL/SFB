518e2358bc586b718e687b910ca76746b538b1a4
GabrielBuergo <gabriel.buergo@fsti360.com>
2026-01-07 13:29:08 -0400

    ers.Configure(wv);

--- SFB.Vue/src/views/pages/iaw/products/components/HeaderBar.vue
+++ SFB.Vue/src/views/pages/iaw/products/components/HeaderBar.vue
@@ -2,13 +2,17 @@
   <header-bar>
     <search-field style="max-width:300px; width:100%" @search="onSearch" />
     <v-spacer />
+    <v-btn color="primary" @click="barcodeScannerRef.openForm()">Escanear</v-btn>
     <v-btn color="primary" @click="emit('crear-producto', 'Insert')">Crear Producto</v-btn>
+    <BarcodeScanner ref="barcodeScannerRef" />
   </header-bar>
 </template>
 
 <script setup>
-import { inject } from 'vue';
+import { inject, ref } from 'vue';
 const { productServ } = inject('services')
+import BarcodeScanner from './BarcodeScanner.vue'
+const barcodeScannerRef = ref(null)
 
 const emit = defineEmits(['crear-producto']);
 
